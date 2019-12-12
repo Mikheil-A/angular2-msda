47b9c4a306bbc2201c00faec57eda900fdbf5be9
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-12-12 18:50:03 +0400

    added gulp task runner for coping assets to dist, added files for angular theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
 
-sass.compiler = require('node-sass');
-
-gulp.task('sass', function() {
-  return gulp.src('./projects/angular-msda/src/lib/core/style/globals.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('./dist/angular-msda-styles'));
-});
-
-gulp.task('sass:watch', function() {
-  gulp.watch('./projects/angular-msda/src/lib/core/style/globals.scss', ['sass']);
+gulp.task('copyLibraryAssetsFolderToDist', function () {
+  return gulp.src('./projects/angular-msda/src/assets/*')
+    .pipe(gulp.dest('./dist/angular-msda/assets/'));
 });
