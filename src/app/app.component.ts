bfe2d26ddd1da5d1a1f9311bccf950346c87c522
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-12-17 15:35:18 +0400

    added routerLinkActive to the navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
+
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  component = 'Components';
 
-    public component = 'Components';
 
-    constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
-    changePage(url: string) {
-        this.router.navigate(['/component/' + url]);
-        this.component = url;
-    }
 
+  changePage(url: string) {
+    // this.router.navigate(['/component/' + url]);
+    this.component = url;
+  }
 }
