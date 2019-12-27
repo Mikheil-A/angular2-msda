985c2ac6d72304a371697f60d1aac4a4d6d60551
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-12-27 13:18:30 +0400

    Auto-generated commit

--- angular-msda/lib/menu-button/menu-button.d.ts
+++
@@ -1,8 +0,0 @@
-import { EventEmitter } from '@angular/core';
-export declare class MSDAMenuButton {
-    active: boolean;
-    text: string;
-    notifCounter: number;
-    click: EventEmitter<any>;
-    onClick(e: Event): void;
-}
