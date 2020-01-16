3487ab4ab1089d4a4000e526ceab305bd3ec445d
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2020-01-16 12:28:57 +0400

    fixed bugs in msda-from-field and msda-button components

--- src/app/components/msda-button/msda-button.component.ts
+++ src/app/components/msda-button/msda-button.component.ts
@@ -1,28 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 
+
+
 @Component({
-    selector: 'app-msda-button',
-    templateUrl: './msda-button.component.html',
-    styleUrls: ['./msda-button.component.scss']
+  selector: 'app-msda-button',
+  templateUrl: './msda-button.component.html',
+  styleUrls: ['./msda-button.component.scss']
 })
 export class MsdaButtonComponent implements OnInit {
-    isLoading = false;
+  isLoading = false;
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    ngOnInit() {
-        this.isLoading = true;
-        setTimeout(() => {
-            this.isLoading = false;
-        }, 3000);
-    }
+  ngOnInit() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
 
-    submit(event) {
-        console.log('clicked', event);
-    }
+  submit(event) {
+    console.log('clicked', event);
+  }
 
-    onError(event) {
-        console.log(event);
-    }
+  onError(event) {
+    console.log(event);
+  }
 }
