7a82c74993449d1714850f729636c33d75f3246d
Aslan Gurbanov <cppleon@gmail.com>
2020-01-21 12:08:57 +0400

    mmdc build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 
+gulp.task('copyMmdcToDist', function () {
+  return gulp.src('./mmdc/dist/*.css')
+    .pipe(gulp.dest('./dist/angular-msda/mmdc/'));
+});
+
 gulp.task('copyMsdaThemeAssetsToDist', function () {
   return gulp.src('./src/msda-theme/*')
     .pipe(gulp.dest('./dist/angular-msda/msda-theme/'));
 });
+
+gulp.task('default', ['copyMmdcToDist', 'copyMsdaThemeAssetsToDist']);
