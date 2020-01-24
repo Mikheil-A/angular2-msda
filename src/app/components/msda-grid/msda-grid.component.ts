eb82df997b27a5f2f83584afdcdcc0e0ec2e40da
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2020-01-24 18:01:31 +0400

    finished working on msda-grid component

--- src/app/components/msda-grid/msda-grid.component.ts
+++ src/app/components/msda-grid/msda-grid.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-msda-grid',
@@ -6,25 +8,102 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./msda-grid.component.scss']
 })
 export class MsdaGridComponent implements OnInit {
-
   ELEMENT_DATA = [
-    {date: '11.02.2017', announcement: 'შენობა-ნაგებობის ექსპლუატაციაში მიღება (პირველადი მიმართვა)', status: 'შუალეუდრი პასუხი', stage: 'ქმედების მოლოდინში', actionDate: '11.02.2017'},
-    {date: '11.02.2017', announcement: 'ჯარიმაა გადასახდელი', status: 'შუალეუდრი პასუხი', stage: 'ქმედების მოლოდინში', actionDate: '11.02.2017'},
-    {date: '11.02.2017', announcement: 'საჟარო ინფორმაციის გამოთხოვა', status: 'შუალეუდრი პასუხი', stage: 'ქმედების მოლოდინში', actionDate: '11.02.2017'},
-    {date: '11.02.2017', announcement: 'ავარიული შენობა ნაგებობის გამაგრება', status: '---', stage: 'ქმედების მოლოდინში', actionDate: '11.02.2017'},
-    {date: '11.02.2017', announcement: 'შენობა-ნაგებობის ექსპლუატაციაში მიღება (პირველადი მიმართვა)', status: 'შუალეუდრი პასუხი', stage: 'ქმედების მოლოდინში', actionDate: '11.02.2017'},
-    {date: '11.02.2017', announcement: 'სპორტული პროექტის', status: 'სერვისის საფასური', stage: 'ქმედების მოლოდინში', actionDate: '11.02.2017'},
-    {date: '11.02.2017', announcement: 'შენობა-ნაგებობის ექსპლუატაციაში მიღება (პირველადი მიმართვა)', status: 'შუალეუდრი პასუხი', stage: 'ქმედების მოლოდინში', actionDate: '11.02.2017'}
+    {
+      plateIcon: {
+        name: 'people',
+        color: '#ea854d'
+      },
+      date: '11.02.2017',
+      announcement: 'შენობა-ნაგებობის ექსპლუატაციაში მიღება (პირველადი მიმართვა)',
+      status: 'შუალეუდრი პასუხი',
+      stage: 'ქმედების მოლოდინში',
+      actionDate: '11.02.2017',
+      actionIcons: ['edit', 'recycle_bin', 'star']
+    },
+    {
+      plateIcon: {
+        name: 'people',
+        color: '#ea854d'
+      },
+      date: '11.02.2017',
+      announcement: 'ჯარიმაა გადასახდელი',
+      status: 'შუალეუდრი პასუხი',
+      stage: 'ქმედების მოლოდინში',
+      actionDate: '11.02.2017',
+      actionIcons: ['edit', 'recycle_bin', 'star']
+    },
+    {
+      plateIcon: {
+        name: 'people',
+        color: '#ea854d'
+      },
+      date: '11.02.2017',
+      announcement: 'საჟარო ინფორმაციის გამოთხოვა',
+      status: 'შუალეუდრი პასუხი',
+      stage: 'ქმედების მოლოდინში',
+      actionDate: '11.02.2017',
+      actionIcons: ['edit', 'recycle_bin', 'star']
+    },
+    {
+      plateIcon: {
+        name: 'people',
+        color: '#e24732'
+      },
+      date: '11.02.2017',
+      announcement: 'ავარიული შენობა ნაგებობის გამაგრება',
+      status: '---',
+      stage: 'ქმედების მოლოდინში',
+      actionDate: '11.02.2017',
+      actionIcons: ['edit', 'recycle_bin', 'star']
+    },
+    {
+      plateIcon: {
+        name: 'people',
+        color: '#87bf29'
+      },
+      date: '11.02.2017',
+      announcement: 'შენობა-ნაგებობის ექსპლუატაციაში მიღება (პირველადი მიმართვა)',
+      status: 'შუალეუდრი პასუხი',
+      stage: 'ქმედების მოლოდინში',
+      actionDate: '11.02.2017',
+      actionIcons: ['edit', 'recycle_bin', 'star']
+    },
+    {
+      plateIcon: {
+        name: 'people',
+        color: '#10a0a2'
+      },
+      date: '11.02.2017',
+      announcement: 'სპორტული პროექტის',
+      status: 'სერვისის საფასური',
+      stage: 'ქმედების მოლოდინში',
+      actionDate: '11.02.2017',
+      actionIcons: ['edit', 'recycle_bin', 'star']
+    },
+    {
+      plateIcon: {
+        name: 'people',
+        color: '#b65452'
+      },
+      date: '11.02.2017',
+      announcement: 'შენობა-ნაგებობის ექსპლუატაციაში მიღება (პირველადი მიმართვა)',
+      status: 'შუალეუდრი პასუხი',
+      stage: 'ქმედების მოლოდინში',
+      actionDate: '11.02.2017',
+      actionIcons: ['edit', 'recycle_bin', 'star']
+    }
   ];
 
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+
   sort(e) {
     console.log(e);
   }
-
 }
